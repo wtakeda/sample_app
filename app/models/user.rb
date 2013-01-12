3e00f43e1dde2ba69d182422b83370654692f69d
unknown <wtakeda@holdenandrew.com>
2013-01-11 16:40:36 -0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
+  before_save :create_remember_token
 #  before_save { |user| user.email = email.downcase }
   before_validation { |user| user.email = email.downcase }
 
@@ -10,6 +11,12 @@ class User < ActiveRecord::Base
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
